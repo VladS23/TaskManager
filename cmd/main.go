95891107eae3db3b75e90b8cb6eda0b871866617
VladS23 <77384665+VladS23@users.noreply.github.com>
2023-08-13 17:43:35 +0300

    add config

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	todo "myTaskManager"
 	"myTaskManager/package/handler"
@@ -9,12 +10,20 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error init config %s", err.Error())
+	}
 	repos := repository.NewRepositoty()
 	services := service.NewService(repos)
 	handler := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatalf("error running server: %s", err.Error())
 	}
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
