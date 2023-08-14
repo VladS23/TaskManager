32acc9c1bfacfb96cc36af70036737ee633d8262
VladS23 <77384665+VladS23@users.noreply.github.com>
2023-08-14 15:13:08 +0300

    add db

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
-	_ "github.com/jackc/pgx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
 	todo "myTaskManager"
 	"myTaskManager/package/handler"
 	"myTaskManager/package/repository"
 	"myTaskManager/package/service"
+	"os"
 )
 
 func main() {
-	repos := repository.NewRepositoty()
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initial config: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error load env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDb(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to init db: %s", err.Error())
+	}
+
+	repos := repository.NewRepositoty(db)
 	services := service.NewService(repos)
 	handler := handler.NewHandler(services)
 
@@ -20,6 +42,7 @@ func main() {
 		log.Fatalf("error running server: %s", err.Error())
 	}
 }
+
 func initConfig() error {
 	viper.AddConfigPath("configs")
 	viper.SetConfigName("config")
--- go.mod
+++ go.mod
@@ -23,6 +23,8 @@ require (
 	github.com/gofrs/uuid v4.4.0+incompatible // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
